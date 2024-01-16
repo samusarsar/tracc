96824bedf99fc7e373823a15fe501fb595fbe9e1
samu.sar <samuil.sarandev@gmail.com>
2024-01-16 13:02:14 +0200

    Add: setup initial routes for landing page and auth routes

--- tracc-frontend/src/app/app.routes.ts
+++ tracc-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then((mod) => mod.AUTH_ROUTES),
+  },
+];
