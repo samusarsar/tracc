e9d43e986d3b1a57a4a7015674d0386ccbd87112
samu.sar <samuil.sarandev@gmail.com>
2024-01-16 17:15:56 +0200

    Refactor: refactored navbar component with tailwind classes

---
+++ tracc-frontend/src/app/navigation/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  @Output() toggleSidenav = new EventEmitter<void>();
+
+  onSidenavToggle() {
+    this.toggleSidenav.emit();
+  }
+}
