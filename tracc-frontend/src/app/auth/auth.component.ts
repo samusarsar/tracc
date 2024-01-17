449eb0da9421d64795c624c63c3904682cec8f14
samu.sar <samuil.sarandev@gmail.com>
2024-01-17 11:26:41 +0200

    Add: added auth routes layout component

--- tracc-frontend/src/app/auth/auth.component.ts
+++ tracc-frontend/src/app/auth/auth.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MatCardModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
--- tracc-frontend/src/app/auth/auth.routes.ts
+++ tracc-frontend/src/app/auth/auth.routes.ts
@@ -1,19 +1,26 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthComponent } from './auth.component';
 
 export const AUTH_ROUTES: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'signup',
-    component: SignupComponent,
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+    ],
   },
 ];
