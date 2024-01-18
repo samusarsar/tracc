d7f56ef27171f168973984867ae342c2fc357469
samu.sar <samuil.sarandev@gmail.com>
2024-01-18 12:39:48 +0200

    Add: added signup flow to signup component using store effects

--- tracc-frontend/src/app/auth/signup/signup.component.ts
+++ tracc-frontend/src/app/auth/signup/signup.component.ts
@@ -13,6 +13,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../../store/app.reducer';
+import * as AuthActions from '../store/auth.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -31,8 +36,17 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class SignupComponent implements OnInit {
   signupForm!: FormGroup;
+  error!: string;
+  isLoading!: boolean;
 
-  constructor(private fb: FormBuilder) {}
+  passwordHidden = true;
+
+  private authStoreSub!: Subscription;
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -40,11 +54,23 @@ export class SignupComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+
+    this.authStoreSub = this.store.select('auth').subscribe((state) => {
+      console.log(state);
+      this.isLoading = state.loading;
+      this.error = state.authError;
+    });
   }
 
-  passwordHidden = true;
-
   onSubmit(form: FormGroup) {
-    console.log(form);
+    if (form.status === 'INVALID' || !form.touched) return;
+
+    this.store.dispatch(
+      AuthActions.signupStart({
+        name: form.value.name,
+        email: form.value.email,
+        password: form.value.password,
+      })
+    );
   }
 }
