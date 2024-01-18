0172d7b8337c56a207afb7bad8284e1ac34c7636
samu.sar <samuil.sarandev@gmail.com>
2024-01-18 17:23:29 +0200

    Add: added basic logout functionality to placeholder dashboard page

--- tracc-frontend/src/app/dashboard/dashboard.component.ts
+++ tracc-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from '../auth/store/auth.actions';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  constructor(private store: Store<fromApp.AppState>) {}
 
+  onLogout() {
+    this.store.dispatch(AuthActions.logout());
+  }
 }
