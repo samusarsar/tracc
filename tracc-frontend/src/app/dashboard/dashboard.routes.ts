20cf6d0d807de5e4e41d6a2901dc00c19c555761
samu.sar <samuil.sarandev@gmail.com>
2024-01-19 16:40:12 +0200

    Add: added dashboard resolver for coinApi requests with dummy data during development

--- tracc-frontend/src/app/dashboard/dashboard.routes.ts
+++ tracc-frontend/src/app/dashboard/dashboard.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { dashboardResolver } from './dashboard.resolver';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    resolve: { coins: dashboardResolver },
   },
 ];
