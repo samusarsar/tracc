cb9ce5409f5cff707de82342ba522658876eeec3
samu.sar <samuil.sarandev@gmail.com>
2024-01-19 16:53:47 +0200

    Add: added initial dashboard page and section

--- tracc-frontend/src/app/dashboard/dashboard.component.ts
+++ tracc-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import * as fromApp from '../store/app.reducer';
-import * as AuthActions from '../auth/store/auth.actions';
+import { ActivatedRoute } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRippleModule } from '@angular/material/core';
+import { UpperCasePipe } from '@angular/common';
 
+import * as AuthActions from '../auth/store/auth.actions';
+import * as fromApp from '../store/app.reducer';
+import { Coin, UserData } from '../shared/types';
+import { CoinCardComponent } from './coin-card/coin-card.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    UpperCasePipe,
+    MatDividerModule,
+    MatRippleModule,
+    CoinCardComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-  constructor(private store: Store<fromApp.AppState>) {}
+export class DashboardComponent implements OnInit {
+  coins!: Coin[];
+  user!: UserData;
+
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.store.select('auth').subscribe((state) => (this.user = state.user!));
+
+    this.activatedRoute.data.subscribe((res) => {
+      this.coins = res['coins'];
+      console.log(this.coins);
+      console.log(this.coins[0].price_change_percentage_24h < 0);
+    });
+  }
 
   onLogout() {
     this.store.dispatch(AuthActions.logout());
