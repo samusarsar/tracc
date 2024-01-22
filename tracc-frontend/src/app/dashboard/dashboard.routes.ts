b1cf4702bf7c7830d02997478d6a45cc1a331baf
samu.sar <samuil.sarandev@gmail.com>
2024-01-22 21:00:04 +0200

    Add: add explore and wallets routes

--- tracc-frontend/src/app/dashboard/dashboard.routes.ts
+++ tracc-frontend/src/app/dashboard/dashboard.routes.ts
@@ -2,11 +2,23 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 import { dashboardResolver } from './dashboard.resolver';
+import { ExploreComponent } from './explore/explore.component';
+import { WalletsComponent } from './wallets/wallets.component';
 
 export const DASHBOARD_ROUTES: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    resolve: { coins: dashboardResolver },
+  },
+  {
+    path: 'explore',
+    component: ExploreComponent,
+    resolve: { coins: dashboardResolver },
+  },
+  {
+    path: 'wallets',
+    component: WalletsComponent,
     resolve: { coins: dashboardResolver },
   },
 ];
