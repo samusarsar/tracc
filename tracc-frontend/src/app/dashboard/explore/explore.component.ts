4919abe85c269310ce12918daac18d4452add220
samu.sar <samuil.sarandev@gmail.com>
2024-01-22 20:59:16 +0200

    Add: add explore page with resolver

---
+++ tracc-frontend/src/app/dashboard/explore/explore.component.ts
@@ -0,0 +1,60 @@
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Coin } from '../../shared/types';
+import { CoinCardComponent } from '../../shared/coin-card/coin-card.component';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-explore',
+  standalone: true,
+  imports: [
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    CoinCardComponent,
+    FormsModule,
+  ],
+  templateUrl: './explore.component.html',
+  styleUrl: './explore.component.scss',
+})
+export class ExploreComponent {
+  coins!: Coin[];
+  filteredCoins!: Coin[];
+
+  searchTerm = '';
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.data.subscribe((res) => {
+      this.coins = res['coins'];
+      this.filteredCoins = this.coins;
+    });
+  }
+
+  handleSearch(value: string) {
+    this.searchTerm = value;
+
+    if (this.searchTerm) {
+      this.filteredCoins = this.coins.filter(
+        (coin) =>
+          coin.name
+            .toLowerCase()
+            .includes(this.searchTerm.trim().toLowerCase()) ||
+          coin.symbol
+            .toLowerCase()
+            .includes(this.searchTerm.trim().toLowerCase())
+      );
+      return;
+    }
+
+    this.filteredCoins = this.coins;
+  }
+}
--- tracc-frontend/src/app/environments/environment.ts
+++ tracc-frontend/src/app/environments/environment.ts
@@ -7,4 +7,6 @@ export const apiRoutes = {
 export const coinApiEndpoints = {
   MARKET_TOP_TEN:
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false',
+  MARKET_TOP_100:
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
 };
