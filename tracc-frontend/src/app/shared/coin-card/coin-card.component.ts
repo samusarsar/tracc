63ea28348db96b01abc2e75e82fdfc8c8b5a367f
samu.sar <samuil.sarandev@gmail.com>
2024-01-23 13:56:53 +0200

    Style: added icons to coin card footer

--- tracc-frontend/src/app/shared/coin-card/coin-card.component.ts
+++ tracc-frontend/src/app/shared/coin-card/coin-card.component.ts
@@ -3,12 +3,20 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatRippleModule } from '@angular/material/core';
 import { UpperCasePipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 import { Coin } from '../../shared/types';
 
 @Component({
   selector: 'app-coin-card',
   standalone: true,
-  imports: [MatCardModule, UpperCasePipe, MatDividerModule, MatRippleModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    UpperCasePipe,
+    MatDividerModule,
+    MatRippleModule,
+  ],
   templateUrl: './coin-card.component.html',
   styleUrl: './coin-card.component.scss',
 })
