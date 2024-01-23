acd08fb524aa0baf4301f009f3160443a907edcd
samu.sar <samuil.sarandev@gmail.com>
2024-01-23 13:57:29 +0200

    Add: added initial wallets page with wallet card grid

--- tracc-frontend/src/app/dashboard/wallets/wallets.component.ts
+++ tracc-frontend/src/app/dashboard/wallets/wallets.component.ts
@@ -1,12 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Coin } from '../../shared/types';
+import { WalletCardComponent } from '../../shared/wallet-card/wallet-card.component';
 
 @Component({
   selector: 'app-wallets',
   standalone: true,
-  imports: [],
+  imports: [WalletCardComponent],
   templateUrl: './wallets.component.html',
-  styleUrl: './wallets.component.scss'
+  styleUrl: './wallets.component.scss',
 })
-export class WalletsComponent {
+export class WalletsComponent implements OnInit {
+  coins!: Coin[];
+  wallets = [
+    {
+      id: 'sdad1',
+      name: 'My Wallet',
+      description: 'This is my wallet',
+      transactions: [
+        {
+          id: '1',
+          coinId: 'bitcoin',
+          buyPrice: 80000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+        {
+          id: '2',
+          coinId: 'bitcoin',
+          buyPrice: 40000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+      ],
+      createdAt: new Date(),
+    },
+    {
+      id: 'sdad1',
+      name: 'My Wallet',
+      description: 'This is my wallet',
+      transactions: [
+        {
+          id: '1',
+          coinId: 'bitcoin',
+          buyPrice: 80000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+        {
+          id: '2',
+          coinId: 'bitcoin',
+          buyPrice: 40000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+      ],
+      createdAt: new Date(),
+    },
+    {
+      id: 'sdad1',
+      name: 'My Wallet',
+      description: 'This is my wallet',
+      transactions: [
+        {
+          id: '1',
+          coinId: 'bitcoin',
+          buyPrice: 80000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+        {
+          id: '2',
+          coinId: 'bitcoin',
+          buyPrice: 40000,
+          buyAmount: 1,
+          createdAt: new Date(),
+        },
+      ],
+      createdAt: new Date(),
+    },
+  ];
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.activatedRoute.parent?.data.subscribe((res) => {
+      this.coins = res['coins']['all'];
+    });
+  }
 }
