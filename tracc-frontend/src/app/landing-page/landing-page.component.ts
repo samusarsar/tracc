43a02de24bfd39a066fd960733314289a4c86965
samu.sar <samuil.sarandev@gmail.com>
2024-01-23 15:52:12 +0200

    Add: add completed landing page

--- tracc-frontend/src/app/landing-page/landing-page.component.ts
+++ tracc-frontend/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import { UserData } from '../shared/types';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  user!: UserData | null;
 
+  constructor(private store: Store<fromApp.AppState>, private router: Router) {}
+
+  ngOnInit() {
+    this.store.select('auth').subscribe((state) => {
+      this.user = state.user;
+    });
+  }
+
+  onNavigate(to: string) {
+    this.router.navigate([to])
+  }
 }
