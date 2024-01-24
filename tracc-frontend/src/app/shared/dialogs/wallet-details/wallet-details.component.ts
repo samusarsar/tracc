63101bd3079e2338d375f254740edd4cf994efde
samu.sar <samuil.sarandev@gmail.com>
2024-01-24 20:55:16 +0200

    Add: added wallet details dialog component

--- tracc-frontend/src/app/shared/dialogs/wallet-details/wallet-details.component.ts
+++ tracc-frontend/src/app/shared/dialogs/wallet-details/wallet-details.component.ts
@@ -1,11 +1,5 @@
-import { Component, Inject, Input } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -18,34 +12,107 @@ import {
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Store } from '@ngrx/store';
-import { Subscription, take } from 'rxjs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 import * as fromApp from '../../../store/app.reducer';
-import * as WalletsActions from '../../../dashboard/wallets/store/wallets.actions';
-import { Wallet } from '../../types';
+import { Coin, Transaction, Wallet } from '../../types';
+import { CreateTransactionComponent } from '../create-transaction/create-transaction.component';
 
 @Component({
   selector: 'app-wallet-details',
   standalone: true,
   imports: [
+    MatChipsModule,
     MatButtonModule,
+    MatIconModule,
     MatDialogActions,
     MatDialogClose,
     MatDialogContent,
     MatDialogTitle,
+    MatDividerModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTableModule,
     FormsModule,
     ReactiveFormsModule,
   ],
   templateUrl: './wallet-details.component.html',
-  styleUrl: './wallet-details.component.scss'
+  styleUrl: './wallet-details.component.scss',
 })
-export class WalletDetailsComponent {
+export class WalletDetailsComponent implements OnInit {
+  wallet!: Wallet;
+
   constructor(
     private fb: FormBuilder,
     private store: Store<fromApp.AppState>,
     private dialog: MatDialog,
-    @Inject(MAT_DIALOG_DATA) public wallet: Wallet
+    private injector: Injector,
+    @Inject(MAT_DIALOG_DATA) public data: { walletId: string; coins: Coin[] }
   ) {}
+
+  ngOnInit() {
+    this.store.select('wallets').subscribe((state) => {
+      this.wallet = state.wallets.find((w) => w.id === this.data.walletId)!;
+    });
+  }
+
+  getFormattedDate(buyDate: string) {
+    return new Date(buyDate).toLocaleDateString();
+  }
+
+  getTransactionCoinInfo(coinId: string) {
+    return this.data.coins.find((c) => c.id === coinId);
+  }
+
+  getTransactionPNL(transaction: Transaction) {
+    const pnl =
+      this.data.coins.find((c) => c.id === transaction.coinId)!.current_price -
+      transaction.buyPrice;
+
+    return {
+      nominal: pnl,
+      percentage: (pnl / transaction.buyPrice) * 100,
+    };
+  }
+
+  getWalletPNL() {
+    let totalInvested = 0;
+    let totalPNL = 0;
+    let totalWeight = 0;
+
+    this.wallet.transactions.forEach((transaction) => {
+      const coin = this.data.coins.find((c) => c.id === transaction.coinId);
+
+      if (coin) {
+        const pnl = coin.current_price - transaction.buyPrice;
+
+        totalInvested += transaction.buyPrice * transaction.buyAmount;
+        totalPNL += pnl * transaction.buyAmount;
+        totalWeight += transaction.buyAmount;
+      }
+    });
+
+    if (totalWeight > 0) {
+      const weightedPNL = (totalPNL / totalInvested) * 100;
+      return {
+        percentage: weightedPNL,
+        nominal: totalPNL,
+      };
+    } else {
+      return {
+        percentage: 0,
+        nominal: 0,
+      };
+    }
+  }
+
+  addTransaction() {
+    this.dialog.open(CreateTransactionComponent, { data: {
+      wallet: this.wallet,
+      coins: this.data.coins
+    }, injector: this.injector });
+  }
 }
