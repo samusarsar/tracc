d89407fd9e1d56d82ec7194d4e221dd1a6bf5700
ssarandev <samuil.sarandev@dxc.com>
2024-02-14 18:25:09 +0200

    Fix: removed unused imports and order component imports

--- tracc-frontend/src/app/navigation/navbar/navbar.component.ts
+++ tracc-frontend/src/app/navigation/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
